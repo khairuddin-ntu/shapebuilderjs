f42c4b357a7bf03b3de411c451b77c00a3f8eb6f
acktor.sg <acktor.sg@gmail.com>
2022-08-16 22:20:32 +0800

    Enabled anti-aliasing on 3D shape

--- src/ShapeRenderer.js
+++ src/ShapeRenderer.js
@@ -9,7 +9,7 @@ export default class ShapeRenderer {
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         const renderer = new THREE.WebGLRenderer({
             canvas: canvasRef,
-            antialias: false,
+            antialias: true,
         });
         renderer.setSize(window.innerWidth, window.innerHeight);
 
