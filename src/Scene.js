112b178d6d580495bb737d07c4d04e0f9b7a8d5c
acktor.sg <acktor.sg@gmail.com>
2022-08-16 23:11:35 +0800

    Create sample render of parametric equation with 1 parameter

--- src/Scene.js
+++ src/Scene.js
@@ -11,6 +11,22 @@ export default class Scene extends React.Component {
     componentDidMount() {
         // Create renderer
         this.renderer = new ShapeRenderer(this.canvasRef.current);
+        this.renderer.renderShape(
+            function (t) {
+                const a = 0.5; // radius
+                return a * Math.sin(t);
+            },
+            function (t) {
+                const b = 0.05; // height per revolution = 2pi * b
+                return b * t;
+            },
+            function (t) {
+                const a = 0.5; // radius
+                return a * Math.cos(t);
+            },
+            { start: -10 * Math.PI, end: 10 * Math.PI },
+            Math.PI / 128
+        );
     }
 
     render() {
--- src/ShapeRenderer.js
+++ src/ShapeRenderer.js
@@ -1,46 +1,94 @@
 import * as THREE from 'three';
 
 export default class ShapeRenderer {
-    constructor(canvasRef) {
-        // Set up scene
-        const scene = new THREE.Scene();
-        scene.background = new THREE.Color(0xffffff);
+    #renderer;
+    #scene;
+    #camera;
+    #group;
 
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        const renderer = new THREE.WebGLRenderer({
+    constructor(canvasRef) {
+        this.#setUpScene(canvasRef);
+        this.#drawGrid();
+        this.#render();
+    }
+
+    renderShape(xEquation, yEquation, zEquation, tRange, tStep) {
+        let xPrev, yPrev, zPrev;
+        let x, y, z;
+        let points, geometry, line;
+        const plotlinematerial = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+
+        for (let t = tRange.start; t <= tRange.end; t += tStep) {
+            x = xEquation(t);
+            y = yEquation(t);
+            z = zEquation(t);
+
+            if (xPrev !== null) {
+                points = [new THREE.Vector3(xPrev, yPrev, zPrev), new THREE.Vector3(x, y, z)];
+                geometry = new THREE.BufferGeometry().setFromPoints(points);
+                line = new THREE.Line(geometry, plotlinematerial);
+                this.#group.add(line);
+            }
+
+            xPrev = x;
+            yPrev = y;
+            zPrev = z;
+        }
+
+        this.#render();
+    }
+
+    #setUpScene(canvasRef) {
+        this.#renderer = new THREE.WebGLRenderer({
             canvas: canvasRef,
             antialias: true,
         });
-        renderer.setSize(window.innerWidth, window.innerHeight);
+        this.#renderer.setSize(window.innerWidth, window.innerHeight);
 
-        // Create lighting, material & shape
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
-        const cube = new THREE.Mesh(geometry, material);
+        this.#scene = new THREE.Scene();
+        this.#scene.background = new THREE.Color(0xffffff);
+
+        this.#camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.#camera.translateZ(10);
+
         const light = new THREE.DirectionalLight();
         light.position.set(-1, 2, 4);
+        this.#scene.add(light);
 
-        // Add lighting & shape to scene
-        scene.add(light);
-        scene.add(cube);
-
-        // Adjust camera position
-        cube.rotateX(Math.PI / 8);
-        cube.rotateY(Math.PI / 4);
-        camera.position.z = 2;
-
-        // Add rendering components as class properties
-        this.scene = scene;
-        this.camera = camera;
-        this.renderer = renderer;
-
-        // Launch render loop
-        this.update();
+        this.#group = new THREE.Group();
+        this.#scene.add(this.#group);
     }
 
-    // ******************* RENDER LOOP ******************* //
-    update() {
-        this.renderer.render(this.scene, this.camera);
-        requestAnimationFrame(this.update);
+    #drawGrid() {
+        const gridMaterial = new THREE.LineBasicMaterial({ color: 0x000000, transparent: true, opacity: 0.1 });
+
+        let points;
+        let geometry;
+        let line;
+
+        for (let x = -10; x <= 10; x++) {
+            for (let y = -10; y <= 10; y++) {
+                for (let z = -1; z <= 1; z++) {
+                    points = [new THREE.Vector3(x, y, z), new THREE.Vector3(x, y, -z)];
+                    geometry = new THREE.BufferGeometry().setFromPoints(points);
+                    line = new THREE.Line(geometry, gridMaterial);
+                    this.#group.add(line);
+
+                    points = [new THREE.Vector3(x, y, z), new THREE.Vector3(x, -y, z)];
+                    geometry = new THREE.BufferGeometry().setFromPoints(points);
+                    line = new THREE.Line(geometry, gridMaterial);
+                    this.#group.add(line);
+
+                    points = [new THREE.Vector3(x, y, z), new THREE.Vector3(-x, y, z)];
+                    geometry = new THREE.BufferGeometry().setFromPoints(points);
+                    line = new THREE.Line(geometry, gridMaterial);
+                    this.#group.add(line);
+                }
+            }
+        }
+    }
+
+    #render() {
+        this.#renderer.render(this.#scene, this.#camera);
     }
 }
