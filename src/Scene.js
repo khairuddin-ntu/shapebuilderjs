0d722737fb59da1194abce9b9f8e38576fda54a3
acktor.sg <acktor.sg@gmail.com>
2022-08-17 15:59:27 +0800

    Generated sample 3D shape

--- src/Scene.js
+++ src/Scene.js
@@ -13,14 +13,27 @@ export default class Scene extends React.Component {
     componentDidMount() {
         // Create renderer
         this.renderer = new ShapeRenderer(this.canvasRef.current);
-        this.renderer.render2dShape(
-            (u) => 10 * u * Math.cos(22 * Math.PI * u),
-            (u) => 10 * u * Math.sin(22 * Math.PI * u),
-            (u) => (10 * u) - 5,
+        //this.renderer.render2dShape(
+        //    (u) => 10 * u * Math.cos(22 * Math.PI * u),
+        //    (u) => 10 * u * Math.sin(22 * Math.PI * u),
+        //    (u) => (10 * u) - 5,
+        //    {
+        //        start: 0, end: 1, get range() { return this.end - this.start }
+        //    },
+        //    200
+        //);
+
+        this.renderer.render3dShape(
+            (u, v) => 5 * Math.cos(2 * Math.PI * u),
+            (u, v) => 5 * Math.sin(2 * Math.PI * u),
+            (u, v) => (11 * v) - 5,
             {
                 start: 0, end: 1, get range() { return this.end - this.start }
             },
-            200
+            {
+                start: 0, end: 1, get range() { return this.end - this.start }
+            },
+            100
         );
     }
 
