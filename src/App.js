1b559307b036afb29d804088edc13ff48e0d3ca1
acktor.sg <acktor.sg@gmail.com>
2022-08-22 15:10:46 +0800

    Created layout for adding text fields & operators sections later

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
+import Box from '@mui/material/Box';
 import Scene from './Scene.js'
 
 import './App.css';
 
-function App() {
+export default function App() {
     return (
-        <div className="App">
-            <Scene/>
-        </div>
+        <Box
+            id="App"
+            sx={{
+                display: "grid",
+                gridTemplateColumns: "repeat(4, 1fr)",
+                gridTemplateRows: "repeat(3, 1fr)"
+            }}
+        >
+            <Box gridColumn="span 3" gridRow="span 2">
+                <Scene />
+            </Box>
+            <Box gridColumn="span 1" gridRow="span 3">
+                {/* TODO: Replace with operators layout */}
+                <Box sx={{
+                    width: 1,
+                    height: 1,
+                    backgroundColor: "lightblue"
+                }} />
+            </Box>
+            <Box gridColumn="span 3">
+                {/* TODO: Replace with text fields layout */}
+                <Box sx={{
+                    width: 1,
+                    height: 1,
+                    backgroundColor: "lightgreen"
+                }} />
+            </Box>
+        </Box>
     );
 }
 
-export default App;
