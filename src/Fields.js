c6384f8cf454473f4d11a58d3b95c7fe7aa787a3
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-08-24 14:23:13 +0800

    Added function fields to layout

--- src/Fields.js
+++ src/Fields.js
@@ -2,16 +2,24 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
+import FunctionField from './FunctionField';
 
 export default function Fields() {
     return (
         <Box sx={{ borderTop: 1 }}>
             <Container>
                 <Grid container>
+                    <FunctionField functionName="x"/>
+                    <Grid item xs={1}></Grid>
+                    <Grid item xs={4}></Grid>
+                    <FunctionField functionName="y"/>
+                    <Grid item xs={1}></Grid>
+                    <Grid item xs={4}></Grid>
+                    <FunctionField functionName="z"/>
                     <Grid item xs={1}>
                         <p>Resolution =</p>
                     </Grid>
-                    <Grid item xs={5}>
+                    <Grid item xs={4}>
                         <TextField
                             variant="outlined"
                             type="number"
