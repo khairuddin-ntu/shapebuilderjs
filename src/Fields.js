309607a27c4b7cba5b38d8db84e0473670faf7cd
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-08-24 14:08:06 +0800

    Aligned fields section to center; Placed resolution label to left of text field

--- src/Fields.js
+++ src/Fields.js
@@ -1,5 +1,25 @@
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
 
 export default function Fields() {
-    return <Box sx={{ borderTop: 1 }}></Box>;
+    return (
+        <Box sx={{ borderTop: 1 }}>
+            <Container>
+                <Grid container>
+                    <Grid item xs={1}>
+                        <p>Resolution =</p>
+                    </Grid>
+                    <Grid item xs={5}>
+                        <TextField
+                            id="resolutionInput"
+                            variant="outlined"
+                            type="number"
+                        />
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
+    );
 }
