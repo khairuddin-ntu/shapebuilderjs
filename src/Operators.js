6364d0481eb1cf92468ba33ee1988615a24e7ebf
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-08-25 13:17:28 +0800

    Added heading for operators

--- src/Operators.js
+++ src/Operators.js
@@ -1,7 +1,12 @@
+import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
 import './Operators.css';
 
 export default function Operators() {
-    return <Box id="operators-container" sx={{ borderLeft: 1 }}></Box>;
+    return (
+        <Box id="operators-container" sx={{ borderLeft: 1 }}>
+            <Typography variant="h4">Operators</Typography>
+        </Box>
+    );
 }
