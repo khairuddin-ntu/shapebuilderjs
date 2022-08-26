aea61335d07c1081374b31dc20016df1441d91c4
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-08-26 16:24:31 +0800

    Added border around functions & parameters section

--- src/Fields.js
+++ src/Fields.js
@@ -12,8 +12,8 @@ export default function Fields(props) {
             {...props}
             sx={{ borderTop: 1 }}
         >
-            <FunctionsSection id="functions-section" />
-            <ParametersSection id="parameters-section" />
+            <FunctionsSection id="functions-section" className="field__section" sectionName="Functions"/>
+            <ParametersSection id="parameters-section" className="field__section" sectionName="Parameters"/>
             <ResolutionSection id="resolution-section" />
             <Box id="actions-section">
                 <Button variant="contained">Generate shape</Button>
