79b5b56498b57cc274c83775b3c5157bb2626973
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-08-26 16:30:55 +0800

    Added button to add additional parameters

--- src/ParametersSection.js
+++ src/ParametersSection.js
@@ -1,16 +1,19 @@
 import Stack from '@mui/material/Stack';
 import ParameterField from './ParameterField';
+import Button from '@mui/material/Button';
+import AddRounded from '@mui/icons-material/AddRounded';
 
 export default function ParametersSection(props) {
     return (
         <Stack
-        {...props}
-        direction="column"
-        spacing={2}
+            {...props}
+            direction="column"
+            spacing={2}
         >
             <ParameterField parameterName="u" />
             <ParameterField parameterName="v" />
             <ParameterField parameterName="w" />
+            <Button startIcon={<AddRounded />}>Add parameter</Button>
         </Stack>
     );
 }
