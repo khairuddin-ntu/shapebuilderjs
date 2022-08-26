cd38d0a7df63f0c6f9e216784d7b143010e0c2cb
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-08-26 16:43:06 +0800

    Displayed parameters based on number of parameters present

---
+++ src/Parameter.js
@@ -0,0 +1,7 @@
+export default class Parameter {
+    name;
+
+    constructor(paramName) {
+        this.name = paramName;
+    }
+}
--- src/ParametersSection.js
+++ src/ParametersSection.js
@@ -1,18 +1,20 @@
+import { useState } from 'react';
 import Stack from '@mui/material/Stack';
 import ParameterField from './ParameterField';
 import Button from '@mui/material/Button';
 import AddRounded from '@mui/icons-material/AddRounded';
+import Parameter from './Parameter';
 
 export default function ParametersSection(props) {
+    const [parameters, setParameters] = useState([new Parameter("u")]);
+
     return (
         <Stack
             {...props}
             direction="column"
             spacing={2}
         >
-            <ParameterField parameterName="u" />
-            <ParameterField parameterName="v" />
-            <ParameterField parameterName="w" />
+            {parameters.map((parameter) => <ParameterField parameterName={parameter.name} />)}
             <Button startIcon={<AddRounded />}>Add parameter</Button>
         </Stack>
     );
