8ecb8c0a7d6e776a1ca1b5c14403ce914b348708
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-08-29 16:49:20 +0800

    Converted Scene component into a function

--- src/render/Scene.js
+++ src/render/Scene.js
@@ -1,32 +1,49 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import ShapeRenderer from './ShapeRenderer';
 
 import './Scene.css';
 
-export default class Scene extends React.Component {
-    #currentMouseX = -1;
-    #currentMouseY = -1;
+export default function Scene(props) {
+    let currentMouseX;
+    let currentMouseY;
+    const renderer = useRef();
 
-    constructor(props) {
-        super(props);
-        this.canvasRef = React.createRef();
-    }
+    const canvasRef = useRef();
 
-    // ******************* COMP`ONENT LIFECYCLE ******************* //
-    componentDidMount() {
-        // Create renderer
-        this.renderer = new ShapeRenderer(this.canvasRef.current);
-        // this.renderer.render2dShape(
-        //    (u) => 10 * u * Math.cos(22 * Math.PI * u),
-        //    (u) => 10 * u * Math.sin(22 * Math.PI * u),
-        //    (u) => (10 * u) - 5,
-        //    {
-        //        start: 0, end: 1, get range() { return this.end - this.start }
-        //    },
-        //    600
-        // );
+    const onStartDrag = (event) => {
+        currentMouseX = event.clientX;
+        currentMouseY = event.clientY;
 
-        this.renderer.render3dShape(
+        canvasRef.current.removeEventListener('mousedown', onStartDrag);
+        canvasRef.current.addEventListener('mousemove', onDrag);
+        canvasRef.current.addEventListener('mouseup', onEndDrag);
+        canvasRef.current.addEventListener('mouseout', onEndDrag);
+    };
+
+    const onDrag = (event) => {
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+
+        const rotationX = mouseX - currentMouseX;
+        const rotationY = mouseY - currentMouseY;
+        renderer.current.rotateShape(rotationX, rotationY);
+
+        currentMouseX = mouseX;
+        currentMouseY = mouseY;
+    };
+    
+    const onEndDrag = () => {
+        canvasRef.current.removeEventListener('mousemove', onDrag);
+        canvasRef.current.removeEventListener('mouseup', onEndDrag);
+        canvasRef.current.removeEventListener('mouseout', onEndDrag);
+        canvasRef.current.addEventListener('mousedown', onStartDrag);
+    };
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+
+        renderer.current = new ShapeRenderer(canvas);
+        renderer.current.render3dShape(
             (u, v) => 5 * Math.cos(2 * Math.PI * u),
             (u, v) => 5 * Math.sin(2 * Math.PI * u),
             (u, v) => (11 * v) - 5,
@@ -39,48 +56,15 @@ export default class Scene extends React.Component {
             100
         );
 
-        this.canvasRef.current.addEventListener('mousedown', this.#onStartDrag);
-    }
+        canvas.addEventListener('mousedown', onStartDrag);
+        return () => canvas.removeEventListener('mousedown', onStartDrag);
+    }, []);
 
-    componentWillUnmount() {
-        this.canvasRef.current.removeEventListener('mousedown', this.#onStartDrag);
-    }
-
-    #onStartDrag = (event) => {
-        this.#currentMouseX = event.clientX;
-        this.#currentMouseY = event.clientY;
-
-        this.canvasRef.current.removeEventListener('mousedown', this.#onStartDrag);
-        this.canvasRef.current.addEventListener('mousemove', this.#onDrag);
-        this.canvasRef.current.addEventListener('mouseup', this.#onEndDrag);
-        this.canvasRef.current.addEventListener('mouseout', this.#onEndDrag);
-    }
-
-    #onDrag = (event) => {
-        const mouseX = event.clientX;
-        const mouseY = event.clientY;
-
-        const rotationX = mouseX - this.#currentMouseX;
-        const rotationY = mouseY - this.#currentMouseY;
-        this.renderer.rotateShape(rotationX, rotationY);
-
-        this.#currentMouseX = mouseX;
-        this.#currentMouseY = mouseY;
-    }
-
-    #onEndDrag = () => {
-        this.canvasRef.current.removeEventListener('mousemove', this.#onDrag);
-        this.canvasRef.current.removeEventListener('mouseup', this.#onEndDrag);
-        this.canvasRef.current.removeEventListener('mouseout', this.#onEndDrag);
-        this.canvasRef.current.addEventListener('mousedown', this.#onStartDrag);
-    }
-
-    render() {
-        return (
+    return (
         <canvas
-        id="canvas"
-        {...this.props}
-        ref={this.canvasRef} />
-        );
-    }
+            id="canvas"
+            {...props}
+            ref={canvasRef}
+        />
+    );
 }
