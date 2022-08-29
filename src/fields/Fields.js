072f178018ee9112de615fb31b8f3a23270a8e05
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-08-29 16:54:18 +0800

    Moved functions & parameters to their own subfolder

--- src/fields/Fields.js
+++ src/fields/Fields.js
@@ -1,7 +1,7 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import FunctionsSection from './FunctionsSection';
-import ParametersSection from './ParametersSection';
+import FunctionsSection from './functions/FunctionsSection';
+import ParametersSection from './parameters/ParametersSection';
 import ResolutionSection from './ResolutionSection'
 
 import './Fields.css';
