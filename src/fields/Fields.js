1e98f2ea3913fde384be9c1c969351c5a4e3049b
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-09-03 11:59:49 +0800

    Moved resolution parsing to resolution Component

--- src/fields/Fields.js
+++ src/fields/Fields.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import MuiAlert from '@mui/material/Alert';
@@ -9,55 +9,19 @@ import ResolutionSection from './ResolutionSection'
 
 import './Fields.css';
 
-export const DEFAULT_RESOLUTION = "100";
-const MAX_RESOLUTION = 2048;
-const REGEX_RESOLUTION = /^\d+$/;
+export const DEFAULT_RESOLUTION = 100;
 
 export default function Fields(props) {
     const [snackbarMessage, setSnackbarMessage] = useState(null);
-
-    const resolutionInput = useRef(DEFAULT_RESOLUTION);
-    const [resolutionError, setResolutionError] = useState(false);
-
-    const onResolutionChange = (resolution) => {
-        setResolutionError(false);
-        resolutionInput.current = resolution;
-    };
-
-    const parseResolution = (strResolution) => {
-        // Check if resolution only contains digits
-        if (!REGEX_RESOLUTION.test(strResolution)) {
-            setResolutionError(true);
-            setSnackbarMessage({ type: "error", text: "Resolution must only contain digits" });
-            return null;
-        }
-
-        const resolution = +strResolution;
-        // Check if resolution is 0
-        if (resolution === 0) {
-            setResolutionError(true);
-            setSnackbarMessage({ type: "error", text: "Resolution cannot be 0" });
-            return null;
-        }
-
-        // Check if resolution is more than maximum allowed resolution
-        if (resolution > MAX_RESOLUTION) {
-            setResolutionError(true);
-            setSnackbarMessage({ type: "error", text: "Resolution cannot be more than " + MAX_RESOLUTION });
-            return null;
-        }
-
-        return resolution;
-    };
+    const [resolution, setResolution] = useState(DEFAULT_RESOLUTION);
 
     const generateShape = () => {
-        console.log("generateShape: Recorded resolution input = ", resolutionInput.current);
-        const resolution = parseResolution(resolutionInput.current);
-        if (resolution === null) {
+        console.log("generateShape: Parsed resolution input = ", resolution);
+        if (isNaN(resolution)) {
+            setSnackbarMessage(resolution);
             return;
         }
 
-        console.log("generateShape: Parsed value of resolution field = ", resolution);
         props.setRenderParams({ resolution: resolution });
     };
 
@@ -70,8 +34,7 @@ export default function Fields(props) {
             <ParametersSection id="parameters-section" className="field__section" sectionName="Parameters" />
             <ResolutionSection
                 id="resolution-section"
-                onResolutionChange={onResolutionChange}
-                resolutionError={resolutionError}
+                setResolution={setResolution}
             />
             <Box id="actions-section">
                 <Button
--- src/fields/ResolutionSection.js
+++ src/fields/ResolutionSection.js
@@ -1,9 +1,44 @@
+import {useState } from 'react';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import { DEFAULT_RESOLUTION } from './Fields';
 
+const MAX_RESOLUTION = 2048;
+const REGEX_RESOLUTION = /^\d+$/;
+
 export default function ResolutionSection(props) {
+    const [hasError, setHasError] = useState(false);
+
+    const onResolutionChange = (event) => {
+        const resolutionInput = event.target.value;
+        props.setResolution(parseResolution(resolutionInput));
+    };
+
+    const parseResolution = (strResolution) => {
+        // Check if resolution only contains digits
+        if (!REGEX_RESOLUTION.test(strResolution)) {
+            setHasError(true);
+            return { type: "error", text: "Resolution must only contain digits" };
+        }
+
+        const resolution = +strResolution;
+        // Check if resolution is 0
+        if (resolution === 0) {
+            setHasError(true);
+            return { type: "error", text: "Resolution cannot be 0" };
+        }
+
+        // Check if resolution is more than maximum allowed resolution
+        if (resolution > MAX_RESOLUTION) {
+            setHasError(true);
+            return { type: "error", text: "Resolution cannot be more than " + MAX_RESOLUTION };
+        }
+
+        setHasError(false);
+        return resolution;
+    }
+
     return (
         <Stack
             id={props.id}
@@ -16,13 +51,13 @@ export default function ResolutionSection(props) {
                 variant="outlined"
                 type="number"
                 defaultValue={DEFAULT_RESOLUTION}
-                error={props.resolutionError}
+                error={hasError}
                 inputProps={{
                     min: 0,
                     max: 999,
                     step: 1
                 }}
-                onChange={(event) => props.onResolutionChange(event.target.value)}
+                onChange={onResolutionChange}
             />
         </Stack>
     );
