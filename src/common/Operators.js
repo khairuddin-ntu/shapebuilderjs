13af194ad20d265a2014b13a10c56e94c12cf5a4
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-09-09 15:39:43 +0800

    Created list of operators

--- src/common/Operators.js
+++ src/common/Operators.js
@@ -14,6 +14,21 @@ class Operator {
     }
 }
 
-class FixedValueOperator extends Operator {}
+class ValueOperator extends Operator {}
 
 class ArithmeticOperator extends Operator {}
+
+export const operatorList = [
+    ArithmeticOperator("Addition", null, null),
+    ArithmeticOperator("Subtraction", null, null),
+    ArithmeticOperator("Multiplication", null, null),
+    ArithmeticOperator("Division", null, null),
+    ValueOperator("Real Number", null, null),
+    ValueOperator("pi", null, null),
+    ValueOperator("Parenthesis", null, null),
+    ValueOperator("Sine", null, null),
+    ValueOperator("Cosine", null, null),
+    ValueOperator("Tangent", null, null),
+    ValueOperator("Fraction", null, null),
+    ValueOperator("Power", null, null)
+];
