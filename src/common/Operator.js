1c07b7f83d8d1333c301ad55fae3e16e755c78e1
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-09-09 16:17:34 +0800

    Moved operator list to Component module

--- src/common/Operator.js
+++ src/common/Operator.js
@@ -16,21 +16,6 @@ class Operator {
     }
 }
 
-class ValueOperator extends Operator { }
+export class ValueOperator extends Operator { }
 
-class ArithmeticOperator extends Operator { }
-
-export const operatorList = [
-    ArithmeticOperator("Addition", null, null),
-    ArithmeticOperator("Subtraction", null, null),
-    ArithmeticOperator("Multiplication", null, null),
-    ArithmeticOperator("Division", null, null),
-    ValueOperator("Real Number", null, null),
-    ValueOperator("pi", null, null),
-    ValueOperator("Parenthesis", null, null),
-    ValueOperator("Sine", null, null),
-    ValueOperator("Cosine", null, null),
-    ValueOperator("Tangent", null, null),
-    ValueOperator("Fraction", null, null),
-    ValueOperator("Power", null, null)
-];
+export class ArithmeticOperator extends Operator { }
--- src/operators/Operators.js
+++ src/operators/Operators.js
@@ -1,7 +1,23 @@
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import { ValueOperator, ArithmeticOperator } from './../common/Operator';
 
 import './Operators.css';
+
+const operatorList = [
+    ArithmeticOperator("Addition", null, null),
+    ArithmeticOperator("Subtraction", null, null),
+    ArithmeticOperator("Multiplication", null, null),
+    ArithmeticOperator("Division", null, null),
+    ValueOperator("Real Number", null, null),
+    ValueOperator("pi", null, null),
+    ValueOperator("Parenthesis", null, null),
+    ValueOperator("Sine", null, null),
+    ValueOperator("Cosine", null, null),
+    ValueOperator("Tangent", null, null),
+    ValueOperator("Fraction", null, null),
+    ValueOperator("Power", null, null)
+];
 
 export default function Operators(props) {
     return (
@@ -13,10 +29,11 @@ export default function Operators(props) {
         >
             <Grid
                 item
-                xs={1}
+                xs={2}
             >
                 <Typography variant="h4">Operators</Typography>
             </Grid>
+
         </Grid>
     );
 }
