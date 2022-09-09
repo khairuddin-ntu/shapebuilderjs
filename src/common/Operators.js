7de754f93e9c5547705c195fbb02845cb3c84ad7
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-09-09 15:59:44 +0800

    Set default UI to be a question mark icon

--- src/common/Operators.js
+++ src/common/Operators.js
@@ -1,22 +1,24 @@
+import QuestionMarkRoundedIcon from '@mui/icons-material/QuestionMarkRounded';
+
 class Operator {
     name;
     generateUi;
     operation;
-    
+
     constructor(name, generateUi, operation) {
         if (this.constructor == Operator) {
             throw new Error("Abstract class cannot be instantiated");
         }
 
         this.name = name;
-        this.generateUi = generateUi;
+        this.generateUi = generateUi ? generateUi : () => <QuestionMarkRoundedIcon />;
         this.operation = operation;
     }
 }
 
-class ValueOperator extends Operator {}
+class ValueOperator extends Operator { }
 
-class ArithmeticOperator extends Operator {}
+class ArithmeticOperator extends Operator { }
 
 export const operatorList = [
     ArithmeticOperator("Addition", null, null),
