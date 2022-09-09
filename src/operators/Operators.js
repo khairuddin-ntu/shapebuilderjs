800ad50ae2801952350ee3f4dfd5c0346ab07ee7
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-09-09 16:07:37 +0800

    Replaced root element with Grid

--- src/operators/Operators.js
+++ src/operators/Operators.js
@@ -1,12 +1,22 @@
+import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 
 import './Operators.css';
 
 export default function Operators(props) {
     return (
-        <Box id={props.id} sx={{ borderLeft: 1 }}>
-            <Typography variant="h4">Operators</Typography>
-        </Box>
+        <Grid
+            container
+            columns={2}
+            id={props.id}
+            sx={{ borderLeft: 1 }}
+        >
+            <Grid
+                item
+                xs={1}
+            >
+                <Typography variant="h4">Operators</Typography>
+            </Grid>
+        </Grid>
     );
 }
