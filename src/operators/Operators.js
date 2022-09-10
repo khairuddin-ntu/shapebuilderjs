96c81f4eea05f2c3e4d8903255a3fda00c681c45
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-09-10 17:25:31 +0800

    Created UI for fraction & power

--- src/operators/Operators.js
+++ src/operators/Operators.js
@@ -1,6 +1,8 @@
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
 import { ValueOperator, ArithmeticOperator, FunctionOperator } from './../common/Operator';
 import OperatorItem from './OperatorItem';
 
@@ -56,8 +58,51 @@ const operatorList = [
         "Tangent", "tan",
         null
     ),
-    new ValueOperator("Fraction", null, null),
-    new ValueOperator("Power", null, null)
+    new ValueOperator(
+        "Fraction",
+        () => (
+            <Stack
+                alignItems="center"
+                spacing={0.7}
+            >
+                <TextField
+                    disabled
+                    size="small"
+                    inputProps={{ size: 1 }}
+                />
+                <hr id="fraction-divider" />
+                <TextField
+                    disabled
+                    size="small"
+                    inputProps={{ size: 1 }}
+                />
+            </Stack>
+        ),
+        null
+    ),
+    new ValueOperator(
+        "Power",
+        () => (
+            <Stack
+                direction="row"
+                spacing={0.5}
+            >
+                <Box mt={2}>
+                    <TextField
+                        disabled
+                        size="small"
+                        inputProps={{ size: 1 }}
+                    />
+                </Box>
+                <TextField
+                    disabled
+                    size="small"
+                    inputProps={{ size: 1 }}
+                />
+            </Stack>
+        ),
+        null
+    )
 ];
 
 export default function Operators(props) {
