30dbc9964e8c712fa356e67b0525ca8026fdbc59
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-09-10 15:59:27 +0800

    Created another operator class for functions

--- src/common/Operator.js
+++ src/common/Operator.js
@@ -1,4 +1,6 @@
 import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
 
 class Operator {
     name;
@@ -23,6 +25,33 @@ export class ArithmeticOperator extends Operator {
         super(
             name,
             () => <Typography variant="h4">{symbol}</Typography>,
+            operation
+        );
+    }
+}
+
+export class FunctionOperator extends Operator {
+    constructor(name, functionName, operation) {
+        super(
+            name,
+            () => (
+                <Stack
+                    direction="row"
+                    alignItems="center"
+                    spacing={0.5}
+                >
+                    <Typography variant="h5">{functionName}(</Typography>
+                    {/* TODO: Resize text field whenever user input changes*/}
+                    <TextField
+                        disabled
+                        size="small"
+                        variant="outlined"
+                        type="text"
+                        inputProps={{ size: 1 }}
+                    />
+                    <Typography variant="h5">)</Typography>
+                </Stack>
+            ),
             operation
         );
     }
