87cb1b791ad02bc3a9aa603d7fa5c005d4d53633
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-09-20 14:17:34 +0800

    Updated decimal regex to include integers

--- src/fields/functions/parser/FunctionParser.js
+++ src/fields/functions/parser/FunctionParser.js
@@ -1,17 +1,17 @@
 import { SnackbarError } from "../../../common/SnackbarMessage";
 import { isEmptyOrBlank } from "../../../common/StringUtils";
 
-const DECIMAL_REGEX = /\d+\.\d+/g;
+const NUMBER_REGEX = /\d+(\.\d+)?/g;
 
 export default function parseFunctionInput(parameters, strInput) {
     console.log("parseFunctionInput: Input = " + strInput);
 
-    // Get all decimals
-    const decimals = [...strInput.matchAll(DECIMAL_REGEX)];
-    console.log(decimals);
+    // Get all numbers
+    const numbers = [...strInput.matchAll(NUMBER_REGEX)];
+    console.log(numbers);
 
     // Get remaining characters after parsing
-    const remainingChars = strInput.split(DECIMAL_REGEX);
+    const remainingChars = strInput.split(NUMBER_REGEX);
     console.log(remainingChars);
 
     for (const chars of remainingChars) {
