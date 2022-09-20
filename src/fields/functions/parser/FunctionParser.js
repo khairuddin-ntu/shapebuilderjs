1c71f808537ed713cd11b20447425aaf430d19c2
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-09-20 14:04:51 +0800

    Extracted all decimal numbers from input

--- src/fields/functions/parser/FunctionParser.js
+++ src/fields/functions/parser/FunctionParser.js
@@ -1,7 +1,13 @@
 import { SnackbarError } from "../../../common/SnackbarMessage";
 
+const DECIMAL_REGEX = /\d+\.\d+/g;
+
 export default function parseFunctionInput(parameters, strInput) {
-    const inputChars = strInput.split("");
-    console.log(inputChars);
+    console.log("parseFunctionInput: Input = " + strInput);
+
+    // Get all decimals
+    const decimals = [...strInput.matchAll(DECIMAL_REGEX)];
+    console.log(decimals);
+
     return [null, new SnackbarError("Parse input not completed")]
 };
