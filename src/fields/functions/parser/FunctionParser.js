93e1ba121613d4c0f42a72fa0a44040b0410cd16
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-09-21 09:59:00 +0800

    Moved code for retrieving tokens to another function

--- src/fields/functions/parser/FunctionParser.js
+++ src/fields/functions/parser/FunctionParser.js
@@ -8,14 +8,22 @@ const MATH_OPERATOR_REGEX = /[+\-*/]/g;
 export default function parseFunctionInput(parameters, strInput) {
     console.log("parseFunctionInput: Input = " + strInput);
 
+    const tokens = getTokens(strInput);
+    console.log(tokens);
+    if (tokens.length === 1) {
+        return [null, tokens[0]];
+    }
+
+    return [null, new SnackbarError("Parse input not completed")];
+};
+
+function getTokens(strInput) {
     // Get all numbers
     const numbers = [];
     let remainingChars = strInput.replace(NUMBER_REGEX, (match, _p1, offset) => {
         numbers.push({ input: match, index: offset });
         return " ".repeat(match.length);
     });
-
-    console.log(numbers);
 
     // Get all instances of pi
     const pis = [];
@@ -24,23 +32,17 @@ export default function parseFunctionInput(parameters, strInput) {
         return " ".repeat(match.length);
     });
 
-    console.log(pis);
-
     // Get all basic mathematical operations
     const operators = [];
     remainingChars = remainingChars.replace(MATH_OPERATOR_REGEX, (match, offset) => {
-        operators.push({input: match, index: offset});
+        operators.push({ input: match, index: offset });
         return " ".repeat(match.length);
     });
-
-    console.log(operators);
 
     // Get remaining characters after parsing
     remainingChars = remainingChars.trim();
     console.log("parseFunctionInput: Remaining characters = " + remainingChars);
-    if (!isEmptyOrBlank(remainingChars)) {
-        return [null, new SnackbarError("Invalid input: " + remainingChars[0])];
-    }
-
-    return [null, new SnackbarError("Parse input not completed")];
-};
+    return isEmptyOrBlank(remainingChars)
+        ? [numbers, pis, operators]
+        : [new SnackbarError("Invalid input: " + remainingChars[0])];
+}
