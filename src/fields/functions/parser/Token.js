9c9549ddef16d1e973b9bcc184c8629ee5aa1e66
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-09-21 13:20:45 +0800

    Created class to represent tokens with children

--- src/fields/functions/parser/Token.js
+++ src/fields/functions/parser/Token.js
@@ -7,3 +7,11 @@ export default class Token {
         this.index = index;
     }
 }
+
+export class WrapperToken {
+    childTokens = [];
+
+    addChildTokens(tokens) {
+        this.childTokens.push(...tokens);
+    }
+}
