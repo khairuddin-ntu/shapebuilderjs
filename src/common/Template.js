fcf5330e59cbcc63f4ab06ea473b292c4f29ed23
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-10-04 11:29:20 +0800

    Created class to represent a template item

---
+++ src/common/Template.js
@@ -0,0 +1,22 @@
+class Template {
+    #name;
+    #imageFileName;
+    #functionInputs;
+    #parameters;
+
+    get name() {
+        return this.#name;
+    }
+
+    get imageFileName() {
+        return this.#imageFileName;
+    }
+
+    get functionInputs() {
+        return this.#functionInputs;
+    }
+
+    get parameters() {
+        return this.#parameters;
+    }
+}
