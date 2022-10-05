b6200ec064eb28d564505b789c1122300f430899
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-10-05 11:49:49 +0800

    Displayed border when hovering over template

--- src/templates/TemplateItem.js
+++ src/templates/TemplateItem.js
@@ -6,6 +6,14 @@ export default function TemplateItem(props) {
 
     return (
         <Stack
+            sx={{
+                border: 2,
+                borderRadius: 2,
+                borderColor: "background.paper",
+                '&:hover': {
+                    borderColor: "primary.main",
+                }
+            }}
             alignItems="center"
             spacing={2}
         >
