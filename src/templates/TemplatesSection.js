c3da98ea9149cd2545893b2eab1a23cd2b7f4679
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-10-05 11:37:52 +0800

    Created template for parametric sweepign

--- src/templates/TemplatesSection.js
+++ src/templates/TemplatesSection.js
@@ -20,6 +20,19 @@ const templates = [
             new Parameter("v")
         ]
     ),
+    new Template(
+        "Parametric Sweeping",
+        "parametric_sweeping.png",
+        [
+            "(0.7u*cos(-4u*pi+pi/2)+1+v)cos(5v*pi+pi)",
+            "0.7u*sin(-4u*pi+pi/2)+4v",
+            "(0.7u*cos(-4u*pi+pi/2)+1+v)sin(5v*pi+pi)"
+        ],
+        [
+            new Parameter("u"),
+            new Parameter("v")
+        ]
+    ),
 ];
 
 export default function TemplatesSection(props) {
@@ -27,6 +40,7 @@ export default function TemplatesSection(props) {
         <Stack
             id={props.id}
             sx={{ borderRight: 1 }}
+            spacing={3}
         >
             <Typography variant="h4">Templates</Typography>
             {templates.map((template, i) => (
