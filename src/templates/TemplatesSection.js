4ac2cf75b626dd7b367a71a88a16bc0375f2b1e8
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-10-05 11:22:45 +0800

    Created template for parametric sphere

--- src/templates/TemplatesSection.js
+++ src/templates/TemplatesSection.js
@@ -1,7 +1,26 @@
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import TemplateItem from './TemplateItem';
+import Template from '../common/Template';
+import Parameter from '../common/Parameter';
 
 import './TemplatesSection.css';
+
+const templates = [
+    new Template(
+        "Sphere",
+        "sphere.png",
+        [
+            "2.5cos(-pi/2+u*pi)cos(-pi+2v*pi)",
+            "2.5cos(-pi/2+u*pi)sin(-pi+2v*pi)",
+            "2.5sin(-pi/2+u*pi)"
+        ],
+        [
+            new Parameter("u"),
+            new Parameter("v")
+        ]
+    ),
+];
 
 export default function TemplatesSection(props) {
     return (
@@ -10,6 +29,9 @@ export default function TemplatesSection(props) {
             sx={{ borderRight: 1 }}
         >
             <Typography variant="h4">Templates</Typography>
+            {templates.map((template, i) => (
+                <TemplateItem templateItem={template}/>
+            ))}
         </Stack>
     );
 }
