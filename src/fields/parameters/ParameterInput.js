c656e9ae3a5d0bf5876f22a13ee3f2cc614f212a
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2022-10-15 17:09:51 +0800

    Changed parameter text field size based on user input length

--- src/fields/parameters/ParameterInput.js
+++ src/fields/parameters/ParameterInput.js
@@ -1,16 +1,26 @@
+import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 
 export default function ParameterInput(props) {
+    const [size, setSize] = useState(1);
+
+    const onUpdate = (event) => {
+        const strInput = event.target.value;
+        const inputLength = strInput.length;
+        setSize(inputLength > 1 ? inputLength - 1 : 1);
+        props.onChange(event.target.value);
+    };
+
     return (
         <TextField
             defaultValue={props.defaultValue}
             error={props.hasError}
             size="small"
             variant="outlined"
-            onChange={(event) => props.onChange(event.target.value)}
+            onChange={onUpdate}
             inputProps={{
                 inputMode: "numeric",
-                size: 8
+                size: size
             }}
         />
     );
